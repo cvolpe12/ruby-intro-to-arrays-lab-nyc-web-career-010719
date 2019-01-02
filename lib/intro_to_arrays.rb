11549c601c8d967cacf6e51b7bf01e42d3dcad2b
Christopher Volpe <christopher.volpe12@gmail.com>
2019-01-02 18:48:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -20,3 +20,13 @@ def last_element
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   taylor_swift[-1]
 end
+
+def first_element_with_array_methods
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  south_east_asia.first
+end
+
+def last_element_with_array_methods
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  south_east_asia.last
+end
